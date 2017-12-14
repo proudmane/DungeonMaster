0909e4f1aaa968234c4e2534bff3032f8cfb85e9
Feliaha <kylelawhorn@gmail.com>
2017-12-14 03:33:01 -0500

    adds xml template

---
+++ DungeonMaster.lua
@@ -0,0 +1,10 @@
+SLASH_DUNGEONMASTER1 = '/dm';
+
+function SlashCmdList.DUNGEONMASTER(msg, editbox)
+  DungeonMasterMainFrame:Show();
+end
+
+function printHello()
+  message('hello world!');
+end
+tyntyn
