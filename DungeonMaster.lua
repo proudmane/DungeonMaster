3326f77cdf6840ae08caa12de4c896365c1f1973
Feliaha <34527602+proudmane@users.noreply.github.com>
2017-12-14 04:08:21 -0500

    enables show/hide commands and proper xml template

--- DungeonMaster.lua
+++ DungeonMaster.lua
@@ -1,10 +1,16 @@
 SLASH_DUNGEONMASTER1 = '/dm';
 
 function SlashCmdList.DUNGEONMASTER(msg, editbox)
-  DungeonMasterMainFrame:Show();
+  local _, _, cmd = string.find(msg, "%s?(%w+)%s?(.*)")
+  if cmd == "show" then
+    DungeonMasterMainFrame:Show();
+  elseif cmd == "hide" then
+    DungeonMasterMainFrame:Hide();
+  else
+    print("Syntax: /dm (show/hide)");
+  end
 end
 
 function printHello()
   message('hello world!');
 end
-tyntyn
